0ad1da866c1faeaaa493cd4a22d057564cebcbfa
anton523 <anton89222910261123@yandex.ru>
2024-06-17 00:19:46 +0500

    1

--- src/Meat.java
+++
@@ -1,6 +0,0 @@
-public class Meat extends Food{
-    public Meat(int taste, int weightInGr) {
-        super(taste, weightInGr);
-    }
-
-}
---
+++ src/MyArrayList.java
@@ -0,0 +1,15 @@
+public class MyList<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private Object[] element;
+    public MyList(){
+        this.element= new Object[DEFAULT_CAPACITY];
+    }
+    public void add(int index, E e){
+        if (index<0 || index>)
+
+    }
+
+
+
+}
